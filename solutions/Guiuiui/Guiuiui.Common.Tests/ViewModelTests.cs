640ad3f70466465118cfc6f38854b9502692ea17
Jonas Aklin <jonas.aklin@faigle.ch>
2020-05-13 15:13:15 +0200

    ViewModelTests added

---
+++ solutions/Guiuiui/Guiuiui.Common.Tests/ViewModelTests.cs
@@ -0,0 +1,23 @@
+﻿
+using Xunit;
+
+namespace Guiuiui.Common.Tests
+{
+    public class ViewModelTests
+    {
+        [Fact]
+        public void SetModel_AnyObject_ValueChangedEventFiredOnce()
+        {
+            // Arrange
+            var candidate = new ViewModel<object>();
+            var eventFired = 0;
+            candidate.ValueChanged += (sender, e) => eventFired++;
+
+            // Act
+            candidate.Model = new object();
+
+            // Assert
+            Assert.Equal(1, eventFired);
+        }
+    }
+}
