c4b89599495011653640ab44569d9fc70e3b93d2
Jonas <jonas.aklin@gmail.com>
2021-04-30 14:28:39 +0200

    Added DateTimePickerAdapterTests

---
+++ solutions/Guiuiui/Guiuiui.WinForms.Tests/Controls/DateTimePickerAdapterTests.cs
@@ -0,0 +1,23 @@
+﻿using Guiuiui.WinForms.Controls;
+using System.Windows.Forms;
+using Xunit;
+
+namespace Guiuiui.WinForms.Tests.Controls
+{
+    /// <summary>
+    /// Performs tests for the <see cref="DateTimePickerAdapter"/> class.
+    /// </summary>
+    public class DateTimePickerAdapterTests
+    {
+        [Fact]
+        public void Dispose_CalledTwice_DoesNotThrow()
+        {
+            // Arrange
+            var candidate = new DateTimePickerAdapter(new DateTimePicker());
+
+            // Act / Assert
+            candidate.Dispose();
+            candidate.Dispose();
+        }
+    }
+}
