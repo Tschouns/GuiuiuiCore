fe848df5b61af6c8a786fc32e605e2a1d1aed22f
Jonas <jonas.aklin@gmail.com>
2021-04-30 14:22:20 +0200

    Added ComboBoxAdapterTests

---
+++ solutions/Guiuiui/Guiuiui.Common.TestTypes/TextConverter/MockTextConverter.cs
@@ -0,0 +1,18 @@
+﻿using Guiuiui.Common.TextConverter;
+
+namespace Guiuiui.Common.TestTypes.TextConverter
+{
+    /// <summary>
+    /// A mock text converter.
+    /// </summary>
+    /// <typeparam name="T">
+    /// See <see cref="ITextConverter{TValue}"/>
+    /// </typeparam>
+    public class MockTextConverter<T> : ITextConverter<T>
+    {
+        public string GetText(T value)
+        {
+            return "mock";
+        }
+    }
+}
---
+++ solutions/Guiuiui/Guiuiui.WinForms.Tests/Controls/ComboBoxAdapterTests.cs
@@ -0,0 +1,25 @@
+﻿
+using Guiuiui.Common.TestTypes.TextConverter;
+using Guiuiui.WinForms.Controls;
+using System.Windows.Forms;
+using Xunit;
+
+namespace Guiuiui.WinForms.Tests.Controls
+{
+    /// <summary>
+    /// Performs tests for the <see cref="ComboBoxAdapter{TValue}"/> class.
+    /// </summary>
+    public class ComboBoxAdapterTests
+    {
+        [Fact]
+        public void Dispose_CalledTwice_DoesNotThrow()
+        {
+            // Arrange
+            var candidate = new ComboBoxAdapter<object>(new ComboBox(), new MockTextConverter<object>());
+
+            // Act / Assert
+            candidate.Dispose();
+            candidate.Dispose();
+        }
+    }
+}
